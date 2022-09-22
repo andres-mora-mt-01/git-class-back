5229e02bf840a58613a0174b9966f7d853e8d653
Andres Mora Vanegas <andresmoradeveloper@gmail.com>
2022-09-22 09:00:10 -0500

    [update] Added new application-prod file Added new route to costume controller

--- src/main/java/com/grupo10/app/rents/CostumeController.java
+++ src/main/java/com/grupo10/app/rents/CostumeController.java
@@ -5,6 +5,7 @@
 package com.grupo10.app.rents;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,11 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
  * @author Andres
  */
 @RestController
-@RequestMapping("/")
+@RequestMapping("/api/Costume")
 public class CostumeController {
     
-    @GetMapping("")
-    public String sayHello(){
-        return "hello group 10";
+    @GetMapping("/all")
+    public String getCostumes(){
+        return "costume 1, costume 2....";
+    }
+    
+    @PostMapping("/save")
+    public String createCostume(){
+        return "crated....";
     }
 }
