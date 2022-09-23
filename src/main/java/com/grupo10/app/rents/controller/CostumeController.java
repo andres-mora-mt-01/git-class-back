4ded12a7a184173b042b6add7cc202e837c7495f
Andres Mora Vanegas <andresmoradeveloper@gmail.com>
2022-09-23 08:51:14 -0500

    [feature] Added quadbike model, repository and controller to call with the api added application properties to dev and prod environments

--- src/main/java/com/grupo10/app/rents/controller/CostumeController.java
+++ src/main/java/com/grupo10/app/rents/controller/CostumeController.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.grupo10.app.rents;
+package com.grupo10.app.rents.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
---
+++ src/main/java/com/grupo10/app/rents/model/Quadbike.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.grupo10.app.rents.model;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author Andres
+ */
+@Entity
+@Table(name="tb_quadbike")
+@Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Quadbike implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY)
+    @Column(name="id")    
+    private Integer id;
+    @Column
+    private String name;
+    @Column
+    private String brand;
+    @Column
+    private Integer year;
+    @Column
+    private String description;
+    @Column
+    private Integer category;  
+    
+    
+    
+}
