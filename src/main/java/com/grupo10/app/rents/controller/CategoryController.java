52cf944c5a27d70912864cd03aa899fa5139964e
Andres Mora Vanegas <andresmoradeveloper@gmail.com>
2022-10-11 09:01:40 -0500

    [feature]
    Added cross origin annotation to handle request from the front end

--- src/main/java/com/grupo10/app/rents/controller/CategoryController.java
+++ src/main/java/com/grupo10/app/rents/controller/CategoryController.java
@@ -9,6 +9,7 @@ import com.grupo10.app.rents.interfaces.ICategoryRepository;
 import com.grupo10.app.rents.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/Category")
+@CrossOrigin(origins = "*")
 public class CategoryController {
 
     @Autowired
