3c010a803ec13cd2230e38ceeec8dd493fed0be0
Andres Mora Vanegas <andresmoradeveloper@gmail.com>
2022-10-13 08:42:10 -0500

    [feature]
    Add cors

--- src/main/java/com/grupo10/app/rents/controller/ReservationController.java
+++ src/main/java/com/grupo10/app/rents/controller/ReservationController.java
@@ -12,6 +12,7 @@ import com.grupo10.app.rents.service.ReservationService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/Reservation")
+@CrossOrigin(origins = "*")
 public class ReservationController {
 
     @Autowired
